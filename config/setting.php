8aaa299844b8d553b1991f361b4edbeb88a068f1
kaburk <github@kaburk.com>
2026-04-23 20:48:17 +0900

    add gitignore config

--- config/setting.php
+++ config/setting.php
@@ -2,7 +2,9 @@
 
 declare(strict_types=1);
 
-return [
+use Cake\Utility\Hash;
+
+$config = [
     'BcApp' => [
         'adminNavigation' => [
             'Plugins' => [
@@ -20,4 +22,23 @@ return [
             ],
         ],
     ],
+    'BcAuthPasskey' => [
+        // RP ID（Relying Party ID）
+        // 通常は未設定で問題ありません。未設定時は現在のホスト名を利用します。
+        // 例: example.com
+        'rpId' => null,
+
+        // RP 名称（Relying Party Name）
+        // パスキー登録時にブラウザ / OS の UI に表示されるサービス名です。
+        // 未設定時は現在のホスト名を利用します。
+        // 例: baserCMS Demo Site
+        'rpName' => null,
+    ],
 ];
+
+if (file_exists(__DIR__ . DS . 'setting_customize.php')) {
+    include __DIR__ . DS . 'setting_customize.php';
+    $config = Hash::merge($config, $customize_config);
+}
+
+return $config;
